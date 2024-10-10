05a27bccfdb96004ffab777ea4c82456527dddbc
anjanc0des <129589930+AnjanC0des@users.noreply.github.com>
2024-10-10 15:58:33 +0000

    add fake server file for chat simul

---
+++ ServerState.tsx
@@ -0,0 +1,53 @@
+export const recipientList = {
+  "5": {
+    name: "Sage",
+    initials: "S",
+  },
+  "4": {
+    name: "John Constantine",
+    initials: "JC",
+  },
+  "3": {
+    name: "Chaos demon",
+    initials: "CD",
+  },
+  "2": {
+    name: "Sauron Dash",
+    initials: "SD",
+  },
+  "1": {
+    name: "Adrian Morgan",
+    initials: "AM",
+  },
+};
+export const messages = {
+  "2": [
+    {
+      id: "101",
+      sender: "2",
+      content: "Hi! How are you!",
+    },
+    {
+      id: "102",
+      sender: "0",
+      content: "Hello! Im doing fine, thank you! How are you?",
+    },
+    {
+      id: "103",
+      sender: "2",
+      content: "Im doing good!",
+    },
+  ],
+  "1": [
+    {
+      id: "201",
+      sender: "0",
+      content: "Hi!",
+    },
+    {
+      id: "202",
+      sender: "1",
+      content: "Dont message me",
+    },
+  ],
+};
